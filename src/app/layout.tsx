c6109fab15d4239778e98613e365b133b2009a99
MOMOKASAN <uttakran.renuman@gmail.com>
2025-10-16 23:44:57 +0700

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MobileProvider } from "@/components/mobile/mobile-provider";
 import { Analytics } from "@vercel/analytics/next"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -92,6 +93,7 @@ export default function RootLayout({
           {children}
         </MobileProvider>
         <Analytics />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""}/>
       </body>
     </html>
   );
