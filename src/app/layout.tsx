623cd5efea1dc3dca1ec959309d0cdb115adf2d8
MOMOKASAN <uttakran.renuman@gmail.com>
2025-10-16 23:26:26 +0700

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MobileProvider } from "@/components/mobile/mobile-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -90,6 +91,7 @@ export default function RootLayout({
         <MobileProvider>
           {children}
         </MobileProvider>
+        <Analytics />
       </body>
     </html>
   );
